ba55f745a8f63e41ad1fd207b0edb3728cc5419d
Ryan Vice <ryanvice@ryans-mbp.attlocal.net>
2017-11-03 13:26:19 -0500

    Extracted store into a seperate file, added dispatch and get state wrappers and added invariant plugin for debug builds.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import './index.css';
 import app from './modules/app';
 import rootReducer from './rootReducer';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from './store';
 // import { Router, Route, browserHistory } from 'react-router';
 
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
-
-const middleware = [
-  thunk,
-];
-
-const enhancer = composeEnhancers(
-  applyMiddleware(...middleware),
-  // other store enhancers if any
-);
-const store = createStore(rootReducer, enhancer);
-
 const { App } = app.components;
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,40 @@
+import { createStore as reduxCreateStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+let store;
+
+export const createStore = (rootReducer, initialState) => {
+  const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      }) : compose;
+
+  const middleware = [
+    thunk,
+  ];
+
+  if (process.env.NODE_ENV !== 'production') {
+    middleware.push(require('redux-immutable-state-invariant').default());
+  }
+
+  const enhancer = composeEnhancers(
+    applyMiddleware(...middleware),
+    // other store enhancers if any
+  );
+
+  store = reduxCreateStore(rootReducer, initialState, enhancer);
+
+  return store;
+};
+
+// Allows access to store.dispatch outside of connected components (e.g. action creators)
+export const dispatch = () => {
+  store.dispatch(arguments);
+};
+
+// Allows access to store.dispatch outside of connected components (e.g. action creators)
+export const getState = () => {
+  store.getState(arguments);
+};
